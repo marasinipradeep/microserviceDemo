552373a2e283336f89ec3898048c616bc5ddf33a
marasinipradeep <marasinipradeep@gmail.com>
2021-03-11 12:22:45 +1030

    removed commits

--- authentication-microservice/index.js
+++ authentication-microservice/index.js
@@ -1,9 +1,3 @@
-// Make sure you replace these values from the copied values of you app
-// const APP_ID = 'f10312b5-63c4-4f76-9dc0-02c3484b907f';
-// const APP_SECERET = 'VktlUbTK.p.Q_9cFJ-yi99OdjDB04acZa9';
-// const TOKEN_ENDPOINT ='https://login.microsoftonline.com/2ef565f8-474e-47ef-aaa4-80d30fb0f79e/oauth2/v2.0/token';
-// const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
-
 require('dotenv').config();
 
 const axios = require('axios');
